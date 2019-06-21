52f6393c4ae6a453ecd44a5b09926c6a5bc8fdad
Shawn Phelps <swolves@yeah.net>
2019-06-21 10:19:36 +0800

    Remove any html tags before php codes returning a image

--- php/ch22/simplegraph.php
+++ php/ch22/simplegraph.php
@@ -1,26 +1,19 @@
-<html>
-    <head>
-        <title>Simple Graph</title>
-    </head>
-    <body>
-        <?php
-        $height = 200;
-        $width = 200;
-        $im = imagecreatetruecolor($width, $height);
-        $white = imagecolorallocate($im, 255, 255, 255);
-        $blue = imagecolorallocate($im, 0, 0, 64);
+<?php
+    $height = 200;
+    $width = 200;
+    $im = imagecreatetruecolor($width, $height);
+    $white = imagecolorallocate($im, 255, 255, 255);
+    $blue = imagecolorallocate($im, 0, 0, 64);
 
-        // draw on image
-        imagefill($im, 0, 0, $blue);
-        imageline($im, 0, 0, $width, $height, $white);
-        imagestring($im, 4, 50, 150, 'Sales', $white);
+    // draw on image
+    imagefill($im, 0, 0, $blue);
+    imageline($im, 0, 0, $width, $height, $white);
+    imagestring($im, 4, 50, 150, 'Sales', $white);
 
-        // output image
-        Header('Content-type: image/png');
-        imagepng($im);
+    // output image
+    Header('Content-type: image/png');
+    imagepng($im);
 
-        // clean up
-        imagedestroy($im);
-        ?>
-    </body>
-</html>
+    // clean up
+    imagedestroy($im);
+?>
